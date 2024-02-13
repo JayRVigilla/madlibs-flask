684dda07616bdc2437b1fac6fcb3afc228654407
Jay Vigilla <jvigilla@avail.io>
2024-02-13 15:53:27 -0800

    1. uses built in stories 2. knits together and outputs a MadLib 3. CSS fix for padding on results

--- app.py
+++ app.py
@@ -7,3 +7,18 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = "secret"
 
 debug = DebugToolbarExtension(app)
+
+@app.get('/')
+def root_screen():
+		""" return initial screen of prompts for a static story"""
+		html = render_template("questions.html", prompts=silly_story.prompts)
+		return html
+
+@app.get('/results')
+def results_screen():
+		""" return  screen of  story knit together"""
+		res = request.args
+		# print("**", res)
+		html = render_template("results.html", story=silly_story.get_result_text(res))
+		return html
+
